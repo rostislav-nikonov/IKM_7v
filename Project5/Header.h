328dfdbb7c2edd1c9daec4c0420bb2bc403c7f5d
rost <rost@LQD>
2025-01-15 21:21:33 +0500

    Добавьте файлы проекта.

---
+++ Project5/Header.h
@@ -0,0 +1,13 @@
+
+#ifndef Header
+#define Header
+
+#include <string>
+using namespace std;
+// Функция для сложения двух больших чисел, представленных в виде строк
+string summ(std::string num1, std::string num2);
+
+// Функция для умножения большого числа (строкой) на целое число
+string umnoj(std::string num, int x);
+
+#endif 
